718ac887bf4a3ab4f73ab82442a9da31b4153c07
samComeIt <sanhae1996@gmail.com>
2020-07-13 13:34:36 +0900

    keycode and eventlistener

---
+++ app.js
@@ -0,0 +1,57 @@
+// use modules -> bc -> keep pieces of code that are related
+
+var bugetController = (function() {
+    
+})();
+
+var UIController = (function() {
+    
+    var DOMstrings = { //private
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue:'.add__value',
+        inputBtn: '.add__btn'
+    };
+    
+    return {
+      getinput: function() {
+          return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+          //return 3 values same time  
+          };
+          
+      },
+        getDOMstrings: function(){ //expose to public
+            return DOMstrings; 
+        }
+    };
+})();
+
+var controller = (function(bugetCtrl, UICtrl) {
+    
+    var DOM = UICtrl.getDOMstrings();
+    
+   var ctrlAddItem = function(){
+       // 1. Get the filed input data
+        var input = UICtrl.getinput();
+       console.log(input);
+        //2. Add the item to the budget controller
+        
+        //3. Add the item to the UI
+        
+        //4. Calculate the budget
+        
+        //5. Display the budget on the UI
+       
+   } 
+   document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13){
+            ctrlAddItem();
+        }
+    });
+    
+})(bugetController, UIController);
