b874430e0c31d3557aae7dfc292cfb51bab09f85
samComeIt <sanhae1996@gmail.com>
2020-07-16 11:33:53 +0900

    Clearing our input fields and focus

--- app.js
+++ app.js
@@ -100,6 +100,19 @@ var UIController = (function() {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
         
+        clearFields: function(){
+            var fields, fieldsArr;
+            
+         fields =  document.querySelectorAll(DOMstrings.inputDescription + ',' + DOMstrings.inputValue);  
+        
+           fieldsArr =  Array.prototype.slice.call(fields);
+            
+            fieldsArr.forEach(function(current, index, array) {
+                current.value ="";
+            });
+            fieldsArr[0].focus();
+        },
+        
         getDOMstrings: function(){ //expose to public
             return DOMstrings; 
         }
@@ -131,7 +144,8 @@ var controller = (function(bugetCtrl, UICtrl) {
         newItem = bugetCtrl.addItem(input.type, input.description, input.value);
         //3. Add the item to the UI
         UICtrl.addListItem(newItem, input.type);
-        
+        // 4. Clear the fields
+        UICtrl.clearFields();
         //4. Calculate the budget
         
         //5. Display the budget on the UI
